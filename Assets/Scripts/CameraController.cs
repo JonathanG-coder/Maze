88adf8b7a4e77dd2bc3fc8c33609327bc59a5661
jonathan <jonathangager75@gmail.com>
2024-08-30 09:50:20 +0200

    Create project Maze

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCotroller : MonoBehaviour
+{
+   [SerializeField] private GameObject target; // Référence à la balle
+   [SerializeField] private Vector3 offset; // Référence à la balle
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
